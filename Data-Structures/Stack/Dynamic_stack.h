d28db0122aac3979485a836c27d26f24e9d54cea
Yanqing Wu <y433wu@uwaterloo.ca>
2017-10-04 21:29:36 -0400

    [upload] ...

--- Data-Structures/Stack/Dynamic_stack.h
+++ Data-Structures/Stack/Dynamic_stack.h
@@ -1,10 +1,27 @@
-/**
- * File              : Dynamic_stack.h
- * Author            : Yanqing Wu <meet.yanqing.wu@gmail.com>
- * Date              : 28.09.2017
- * Last Modified Date: 28.09.2017
- * Last Modified By  : Yanqing Wu <meet.yanqing.wu@gmail.com>
- */
+/*****************************************
+ * Instructions
+ *  - Replace 'uwuserid' with your uWaterloo User ID
+ *  - Select the current calendar term and enter the year
+ *  - List students with whom you had discussions and who helped you
+ *
+ * uWaterloo User ID:  y433wu @uwaterloo.ca
+ * Submitted for ECE 250
+ * Department of Electrical and Computer Engineering
+ * University of Waterloo
+ * Calender Term of Submission:  (Fall) 2017
+ *
+ * By submitting this file, I affirm that
+ * I am the author of all modifications to
+ * the provided code.
+ *
+ * The following is a list of uWaterloo User IDs of those students
+ * I had discussions with in preparing this project:
+ *    -
+ *
+ * The following is a list of uWaterloo User IDs of those students
+ * who helped me with this project (describe their help; e.g., debugging):
+ *    -
+ *****************************************/
 
 #ifndef DYNAMIC_STACK_H
 #define DYNAMIC_STACK_H
@@ -16,6 +33,7 @@
 #include "Exception.h"
 #include <iostream>
 using namespace std;
+
 
 class Dynamic_stack {
 
@@ -44,7 +62,14 @@ count( 0 ),
 array_size( n ),
 initial_size(n)
 {
+    if (n < 0) {
+        // std::cout << "Detected negative array size input!" << std::endl;
+        array_size = 1;
+        initial_size = 1;
+    }
+    std::cout << "array hasn't been initialized, should have nothing: " << array << std::endl;
 	array = new int[array_size];
+    std::cout << "array has been initialized, should have an address: " << array << std::endl;
 }
 
 
@@ -89,25 +114,30 @@ int Dynamic_stack::capacity() const {
 }
 
 
-void Dynamic_stack::push( int const &obj ) {   
+void Dynamic_stack::push( int const &obj ) {
+    // std::cout << "Detect one input: " << obj << std::endl;  // for debugging
+    
     // if the array is full, create a new array with size 2n
     // and copy the elements from the current array to the new array in O(n)
 
     if (size() == capacity()) {
-        std::cout << "Reaches current limit capacity, will double capacity right now." << std::endl;
         int * newArray = new int[capacity()];    // create a temporary array for copying
 
         for (int i = 0; i < capacity(); i++) {
             newArray[i] = array[i];
+            // std::cout << "array[" << i << "] address " << &array[i] << std::endl;
         }
         delete [] array;
+        std::cout << "array has been deallocated, should have nothing: " << array << std::endl;
 
         array = new int[2*capacity()];
         for (int i = 0; i < capacity(); i++) {
             array[i] = newArray[i];
+            // std::cout << "newArray[" << i << "] address " << &newArray[i] << std::endl;
         }
         delete [] newArray;
-        
+        std::cout << "newArray has been deallocated, should have nothing: " << array << std::endl;
+
         array_size *= 2;
     }
 
@@ -131,17 +161,32 @@ int Dynamic_stack::pop() {
 }
 
 void Dynamic_stack::clear() {
+	// Enter your implementation here 
     // if current array size does not equal the initial size, delete the array
     // and create a new array equal to the initial size
     
     // Re-initialize the counter
     count = 0;
+    // Re-initialize the array_size
     if (array_size != initial_size) {
-        array_size = 10;
+        array_size = initial_size;
     }
 
     // Free memory
-    delete [] array;
+    if (array == NULL) {
+        // std::cout << "array is null." << std::endl;
+    }
+    else {
+        // std::cout << "array is not null." << std::endl;
+        // std::cout << array << std::endl;
+        array = 0;
+        // if (empty() != true) {
+        //     std::cout << "It's not empty!" << std::endl;
+        //     delete [] array;
+        // }
+    }
+    // delete [] array; // If including this, will have delete same address array;
+                        // memory is not cleaned
     
 }
 #endif
